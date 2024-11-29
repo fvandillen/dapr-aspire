a9aebc12786322c377db0f214cdb2d0b53ecac85
Florian van Dillen <f.vandillen@4dotnet.nl>
2024-11-29 16:02:11 +0100

    Some optimizations.

--- DaprAspire/DaprAspire.Services.RegulatoryInspector/Database/RegulatoryInspectorContext.cs
+++ DaprAspire/DaprAspire.Services.RegulatoryInspector/Database/RegulatoryInspectorContext.cs
@@ -2,12 +2,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DaprAspire.Services.RegulatoryInspector.Database;
 
-public class RegulatoryInspectorContext : DbContext
+public class RegulatoryInspectorContext(DbContextOptions<RegulatoryInspectorContext> options) : DbContext(options)
 {
 	public DbSet<Violation> Violations { get; set; }
-
-	public RegulatoryInspectorContext(DbContextOptions<RegulatoryInspectorContext> options) : base(options)
-	{
-		
-	}
 }
